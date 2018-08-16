e449634e80f37c645fc5006c291232285d75dadb
Felix Bohna <fb@holsteinmedia.com>
2018-08-16 14:23:34 +0200

    bugfix not able to create elements in tree sorting

--- Resources/contao/Helper/Dca/Sortings/Tree.php
+++ Resources/contao/Helper/Dca/Sortings/Tree.php
@@ -20,19 +20,11 @@ class Tree extends \Backend
         'mode' => 5,
         'fields' => ['name'],
         'headerFields' => ['name'],
-        'panelLayout' => 'debug;filter;sort,search,limit',
-        'paste_button_callback'   => array('Home\PearlsBundle\Resources\contao\Helper\Dca\Sortings\Tree', 'pasteElement')
+        'panelLayout' => 'debug;filter;sort,search,limit'
     );
 
     public static function getSettings()
     {
         return self::SETTINGS;
-    }
-
-    public function pasteElement($dc, $row, $table)
-    {
-        $imagePasteAfter = \Image::getHtml('pasteafter.gif', sprintf($GLOBALS['TL_LANG'][$table]['pasteafter'][1], $row['id']));
-        return '<a href="'.$this->addToUrl('act=cut&mode=1&pid='.$row['id']).'" title="'.specialchars(sprintf($GLOBALS['TL_LANG'][$table]['pasteafter'][1], $row['id'])).'" onclick="Backend.getScrollOffset()">'.$imagePasteAfter.'</a> ';
-
     }
 }
