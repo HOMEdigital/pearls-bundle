91a9c079932bcb538dd7c56eab28b4441e687c46
Felix Bohna <fb@holsteinmedia.com>
2019-03-28 10:44:57 +0100

    Date field update

--- Resources/contao/Helper/Dca/Fields/Date.php
+++ Resources/contao/Helper/Dca/Fields/Date.php
@@ -21,13 +21,37 @@ class Date extends Base
         'eval' => array(
             'rgxp'=>'datim',
             'datepicker'=>true,
-            'tl_class'=>'w50 wizard'
+            'tl_class'=>'w50 wizard clr'
         ),
-        'sql' => "varchar(255) NOT NULL default ''",
+        'sql' => "int(10) unsigned NOT NULL default '0'",
+        'save_callback' => array(
+            array('Home\PearlsBundle\Resources\contao\Helper\Dca\Fields\Date','onSave'),
+        ),
+        'load_callback' => array(
+            array('Home\PearlsBundle\Resources\contao\Helper\Dca\Fields\Date','onLoad'),
+        ),
     );
 
     public static function getSettings()
     {
         return array_replace_recursive(parent::getSettings(), self::SETTINGS);
+    }
+
+    public static function onLoad($varValue, $dc)
+    {
+        if($varValue && $varValue !== 0){
+            return $varValue;
+        }else{
+            return time();
+        }
+    }
+
+    public static function onSave($varValue, $dc)
+    {
+        if($varValue && $varValue !== 0){
+            return $varValue;
+        }else{
+            return time();
+        }
     }
 }
