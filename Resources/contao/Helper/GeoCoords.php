51f9b1b755c7ffdd032d7642eeb880f89931215e
Felix Bohna <fb@holsteinmedia.com>
2019-06-26 14:40:58 +0200

    update GeoCoords form google maps to osm

--- Resources/contao/Helper/GeoCoords.php
+++ Resources/contao/Helper/GeoCoords.php
@@ -2,36 +2,56 @@
 
 namespace Home\PearlsBundle\Resources\contao\Helper;
 
-class GeoCorrds
+class GeoCoords
 {
 
     /**
      * getGeoCoordsFromGoogleByAddress
-     * get geo coordinates from goolge by an address
-     * @param string address - 'street zip city'
-     * @param string country - the country like 'de'
-     * @return array with 'lat' and 'lng' values
+     * get geo coordinates from OSM by an address
+     * @param string address - 'street,zip,city'
+     * @param string $countryCode - must be the ISO 3166-1alpha2 code
+     * @return array - with 'lat' and 'lon' values
      */
-    public static function getGeoCoordsFromGoogleByAddress($address, $country = 'de') {
+    public static function getGeoCoordsByAddress($address, $countryCode = 'de') {
         if (!is_string($address)) return false;
 
-        $googleUrl = 'https://maps.google.com/maps/api/geocode/xml';
-        $strGeoUrl = $googleUrl . '?address=' .str_replace(' ', '+', $address) . '&region=' . $country . '&sensor=false';
+        #-- base url for request
+        $osmUrl = 'https://nominatim.openstreetmap.org/search.php';
+        #-- set params
+        $strGeoUrl = $osmUrl . '?q=' .str_replace(' ', '%20', $address) . '&countrycodes=' . $country .
+            '&limit=1&format=json';
 
-        // get geo coordinates
+        #-- check if curl is enabled on server
         if (function_exists("curl_init")) {
+            #-- init curl
             $curl = curl_init();
             if ($curl) {
-                if (curl_setopt($curl, CURLOPT_URL, $strGeoUrl) && curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1) && curl_setopt($curl, CURLOPT_HEADER, 0)) {
-                    $curlVal = curl_exec($curl);
-                    curl_close($curl);
+                #-- set curl options
+                curl_setopt_array($curl, array(
+                    CURLOPT_URL => $strGeoUrl,
+                    CURLOPT_RETURNTRANSFER => true,
+                    CURLOPT_TIMEOUT => 30,
+                    CURLOPT_CUSTOMREQUEST => "GET",
+                    CURLOPT_REFERER => (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}",
+                    CURLOPT_HTTPHEADER => array(
+                        "cache-control: no-cache"
+                    ),
+                ));
 
-                    $xml = new \SimpleXMLElement($curlVal);
-                    if($xml) {
-                        return array(
-                            'lat'=>$xml->result->geometry->location->lat,
-                            'lng'=>$xml->result->geometry->location->lng);
-                    }
+                #-- get response from curl
+                $response = curl_exec($curl);
+                $err = curl_error($curl);
+                #-- close curl connection
+                curl_close($curl);
+                #-- decode received json
+                $json = json_decode($response, true);
+
+                if ($json && is_array($json) && $json[0] && $json[0]['lat'] && $json[0]['lon']) {
+                    #-- return latitude and longitude
+                    return array(
+                        'lat' => $json[0]['lat'],
+                        'lon' => $json[0]['lon']
+                    );
                 }
             }
         }
@@ -41,17 +61,17 @@ class GeoCorrds
 
     /**
      * autoCenterMap
-     * return the center lat and lng and zoom level from all data coords to center an google map
+     * return the center lat and lon and zoom level from all data coords to center an OSM map
      * @param array $data - coordinates from all data
      * @param array $myCoord [Null] - coordinates from own location
      * @param string $geocoords ['geocoords'] - the key string where the geocoordinates are stored in the $data-array
-     * @return array with 'lat', 'lng' and 'zoom'
+     * @return array with 'lat', 'lon' and 'zoom'
      */
     public static function autoCenterMap($data, $myCoord=NULL, $geocoordsName='geocoords') {
         $minLat = ($myCoord != NULL) ? floatval($myCoord['lat']) : 0;
         $maxLat = ($myCoord != NULL) ? floatval($myCoord['lat']) : 0;
-        $minLng = ($myCoord != NULL) ? floatval($myCoord['lng']) : 0;
-        $maxLng = ($myCoord != NULL) ? floatval($myCoord['lng']) : 0;
+        $minLng = ($myCoord != NULL) ? floatval($myCoord['lon']) : 0;
+        $maxLng = ($myCoord != NULL) ? floatval($myCoord['lon']) : 0;
         $centerLat = 0;
         $centerLng = 0;
         $zoom = 0;
@@ -100,7 +120,7 @@ class GeoCorrds
 
         return array(
             "lat" => $centerLat,
-            "lng" => $centerLng,
+            "lon" => $centerLng,
             "zoom" => $zoom
         );
     }
